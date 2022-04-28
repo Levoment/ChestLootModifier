a36e9b574dee033b8cea3b5b09e185752e5d4196
Levoment <30224138+Levoment@users.noreply.github.com>
2022-04-28 01:35:12 -0500

    Change mixin json name

--- src/main/java/com/github/levoment/chestlootmodifier/ChestLootModifierMod.java
+++ src/main/java/com/github/levoment/chestlootmodifier/ChestLootModifierMod.java
@@ -27,12 +27,6 @@ public class ChestLootModifierMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
-		LOGGER.info("Hello Fabric world!");
-
 		// Create the config file if it doesn't exist
 		ConfigManager.createConfigFile();
 		// Read the config file if it exists
